5e13af48f802fe8b3f99275ee9e0f6cbba412d10
Chandan Ray <chandan.ray@digital.hmrc.gov.uk>
2026-05-05 16:39:49 +0100

    Update CustomerAdvisorsFrontendSpec.scala

--- src/test/scala/uk/gov/hmrc/ui/specs/customerAdvisorsTests/CustomerAdvisorsFrontendSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/customerAdvisorsTests/CustomerAdvisorsFrontendSpec.scala
@@ -17,7 +17,7 @@
 package uk.gov.hmrc.ui.specs.customerAdvisorsTests
 
 import uk.gov.hmrc.ui.pages.authWizard.LoginUsingAuthWizardPage
-import uk.gov.hmrc.ui.pages.authWizard.LoginUsingAuthWizardPage.{lonIntoCustomerAdvisorMessageInboxPage, lonIntoCustomerAdvisorMessageSautrPage}
+import uk.gov.hmrc.ui.pages.authWizard.LoginUsingAuthWizardPage.{logIntoCustomerAdvisorMessageInboxPage, logIntoCustomerAdvisorMessageSautrPage}
 import uk.gov.hmrc.ui.pages.customerAdvisorsFrontend.customerAdvisorsMessagePage
 import uk.gov.hmrc.ui.pages.customerAdvisorsFrontend.customerAdvisorsMessagePage.responseMessage
 import uk.gov.hmrc.ui.pages.messages.MdtpMessages.*
@@ -31,7 +31,7 @@ class CustomerAdvisorsFrontendSpec extends BaseSpec {
 
     Scenario("Customer advisors can submit a valid message to a particular customer", OwsmTests) {
       Given("I navigate to customer advisors page")
-      lonIntoCustomerAdvisorMessageInboxPage()
+      logIntoCustomerAdvisorMessageInboxPage()
       When("I fill the form for FHDDS message and click send")
       createMDTPMessage("FHDDS", "valid")
       Then("I should see that message is created")
@@ -40,7 +40,7 @@ class CustomerAdvisorsFrontendSpec extends BaseSpec {
 
     Scenario("Customer advisors can't submit a invalid message to a particular customer", OwsmTests) {
       Given("I navigate to customer advisors page")
-      lonIntoCustomerAdvisorMessageInboxPage()
+      logIntoCustomerAdvisorMessageInboxPage()
       When("I fill the form for FHDDS message and click send")
       createMDTPMessage("FHDDS", "invalid")
       Then("I should see that message is created")
@@ -49,7 +49,7 @@ class CustomerAdvisorsFrontendSpec extends BaseSpec {
 
     Scenario("Customer advisors can't submit a valid duplicate message to a particular customer", OwsmTests) {
       Given("I navigate to customer advisors page")
-      lonIntoCustomerAdvisorMessageInboxPage()
+      logIntoCustomerAdvisorMessageInboxPage()
       When("I fill the form for FHDDS message and click send")
       createMDTPMessage("FHDDS", "valid")
       And("I resubmit the same form once again with same data")
@@ -69,7 +69,7 @@ class CustomerAdvisorsFrontendSpec extends BaseSpec {
       PaperlessEmailPage.fillEmailPage()
       verifyEmail()
       When("I navigate to customer advisors page")
-      lonIntoCustomerAdvisorMessageSautrPage("correct")
+      logIntoCustomerAdvisorMessageSautrPage("correct")
       And("I fill the form to reply and click send")
       customerAdvisorsMessagePage.fillFormToReply()
       Then("I should see that message is submitted successfully")
@@ -88,7 +88,7 @@ class CustomerAdvisorsFrontendSpec extends BaseSpec {
       PaperlessEmailPage.fillEmailPage()
       verifyEmail()
       When("I navigate to customer advisors page")
-      lonIntoCustomerAdvisorMessageSautrPage("wrong")
+      logIntoCustomerAdvisorMessageSautrPage("wrong")
       And("I fill the form to reply and click send")
       customerAdvisorsMessagePage.fillFormToReply()
       Then("I should see the message after submission")
