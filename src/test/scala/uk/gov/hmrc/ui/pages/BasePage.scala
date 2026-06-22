93d3da49edf526e349a6fa2f0a3cd999822af4ba
anjalipnair <anjalipnair2020@gmail.com>
2026-06-22 14:06:58 +0100

    DC-8946_EpsMessages

--- src/test/scala/uk/gov/hmrc/ui/pages/BasePage.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/BasePage.scala
@@ -46,7 +46,7 @@ trait BasePage extends PageObject with TestData with ApiPayLoad {
 
   implicit val system: ActorSystem = ActorSystem()
 
-  val WsClient = StandaloneAhcWSClient()
+  val WsClient: StandaloneAhcWSClient = StandaloneAhcWSClient()
 
   val entityIdRegex = "(\\bJsDefined\\b|\\(|\\)|\")"
 
@@ -212,5 +212,37 @@ trait BasePage extends PageObject with TestData with ApiPayLoad {
   def waitForText(selector:String, text: String): Unit = {
     fluentWait.until(driver => driver.findElement(By.cssSelector(selector)).getText.equals(text))
 
+  }
+
+  def postCDSMessage(payload: String): Unit = {
+    val response = Await.result(
+      WsClient
+        .url(message)
+        .addHttpHeaders("Content-Type" -> "application/json")
+        .post(payload),
+      5.seconds
+    )
+    println(response.status)
+    assert(response.status == 201)
+  }
+
+  def suppressionDataToNpsThruHip(scenario: String): Unit = {
+    val payload = scenario match {
+      case "digital" => digitalSuppressionDataToNpsThruHip
+      case "paper" => paperSuppressionDataToNpsThruHip
+      case "bounced" => bouncedSuppressionDataToNpsThruHip
+      case _ => throw new IllegalArgumentException(s"Unknown scenario: $scenario")
+    }
+
+    val p2HipNpsUrl = digitalContactStub + "/paye/individual/print-preferences"
+
+    val response = Await.result(
+      WsClient
+        .url(p2HipNpsUrl)
+        .addHttpHeaders("Content-Type" -> "application/json")
+        .post(payload),
+      5.seconds
+    )
+    assert(response.status == 200)
   }
 }
